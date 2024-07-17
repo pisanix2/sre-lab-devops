bc35325b9a379dc0628d2d11027f07a0fbcb2a63
Rafael Pisani <pisanix@gmail.com>
2024-07-17 18:04:51 -0300

    feat: primeira versao do codigo

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
